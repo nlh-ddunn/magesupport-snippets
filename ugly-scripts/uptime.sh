6e4d2dc01a2a120d6f243ef2f6fcd39e28693c28
David Dunn <dadunn@adobe.com>
2019-08-29 09:42:17 -0500

    Added uptime.sh and added README for ugly scripts

---
+++ ugly-scripts/uptime.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# file: uptime.sh
+# usage uptime.sh ent-HASH-production-stuff@ssh.REGION.magento.cloudi
+# ie: everything after the 1,2,3. in the connect to the ssh in the cloud UI
+# Note: this assumes a 3 node cluster
+
+for i in {1..3}
+do
+   echo -en "\e[91m$i\e[32m.$1\n\e[97m---\n"
+   ssh $i.$1 'uptime'
+   echo -en "\033[0m\n"
+done
